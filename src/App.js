c6f3a8a16d9369fb7fd48242b365b051f795a7dc
Mehul Singh <mehul619b@gmail.com>
2020-12-19 20:41:36 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios'
 
 function App() {
-  return (
+  const [inputFile,setinputFile]=useState(null);
+  const [receivedFileName,setreceivedFileName]=useState("");
+  const postFile=async (e)=>{
+    e.preventDefault();
+    const data=new FormData();
+    data.append('file',inputFile);
+
+    const res=await axios.post('https://inspekt-assignment.herokuapp.com/save',data);
+    setreceivedFileName(res.data);
+  }
+  const postUrl="https://inspekt-assignment.herokuapp.com/save";
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form>
+          <input type="file" onChange={(e)=>(setinputFile(e.target.files[0]))}/>
+          <button onClick={postFile} type="submit">Submit</button>
+        </form>
+        <br/>
+        {receivedFileName!=""? <label>FILENAME: {receivedFileName}</label>:""}
+      </div>
     </div>
   );
 }
